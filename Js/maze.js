04991b5e7a20f0e38c04dd9360c07de764dd5faa
ThanhLuan <luan.tt87@gmail.com>
2020-03-12 14:46:17 -0600

    1
    1

---
+++ Js/maze.js
@@ -0,0 +1,41 @@
+
+var touchwall=0;
+$(document).ready(function(){
+	
+	
+	var mouseOverWall=function(){
+	  // $(this).css({"background-color":"red"})
+	  $(this).addClass("youlose");
+	  touchwall=1;
+	}
+	
+	var mouseOverEnd=function(){
+		if(touchwall===1){
+		  //alert("You win! :]");
+		  $("#status").text("You win! :]");
+		}
+		else
+		  $("#status").text("You lose");
+	}
+	
+	var mouseOverStart=function(){
+		 touchwall=1;
+		 $(".boundary").removeClass ("youlose");
+		 
+		 $("#status").text("Click the 'S' to begin.");
+		 
+	}
+	
+	var onMouseOut = function(){
+		touchwall=0;
+		console.log(1);
+	}
+	
+	
+	
+	
+  $(".boundary").mouseover(mouseOverWall);
+  $("#end").mouseover(mouseOverEnd);
+  $("#maze").mouseleave(onMouseOut);
+  $("#start").click(mouseOverStart);  
+})
